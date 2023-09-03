5d2f0a10e244608958be3b3d7bea25089a75e99a
Karapetyan, Aram <aram@dasmeta.com>
2023-09-03 19:54:14 +0400

    Manually fix escape chars to get job rolling

--- _terraform/2-products/product-1/dev-1/ingress/main.tf
+++ _terraform/2-products/product-1/dev-1/ingress/main.tf
@@ -4,15 +4,15 @@ module "this" {
   source  = "dasmeta/modules/aws//modules/ingress"
   version = "1.1.4"
 
-  certificate_arn = "${data.tfe_outputs.this[\"2-products/product-1/certificate\"].values.results.output.arn}"
-  healthcheck_path = "/health"
+  certificate_arn           = data.tfe_outputs.this["2-products/product-1/certificate"].values.results.output.arn
+  healthcheck_path          = "/health"
   healthcheck_success_codes = "200-399"
-  hostname = "test.dasmeta.com"
-  name = "test"
-  namespace = "dev"
-  scheme = "internal"
-  tls_hosts = ["test.dasmeta.com"]
-  providers = {"aws":"aws"}
+  hostname                  = "test.dasmeta.com"
+  name                      = "test"
+  namespace                 = "dev"
+  scheme                    = "internal"
+  tls_hosts                 = ["test.dasmeta.com"]
+  providers                 = { "aws" : "aws" }
 }
 
 

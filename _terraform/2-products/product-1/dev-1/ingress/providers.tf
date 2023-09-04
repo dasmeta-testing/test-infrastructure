a19462e41259690cb2815a35f3fb82327c2922d7
Karapetyan, Aram <aram@dasmeta.com>
2023-09-04 10:09:38 +0400

    Fix provider backslash

--- _terraform/2-products/product-1/dev-1/ingress/providers.tf
+++ _terraform/2-products/product-1/dev-1/ingress/providers.tf
@@ -8,24 +8,24 @@ provider "aws" {
 
   default_tags {
 
-    tags = {"Account":"boilerplate","AppliedFrom":"terraform-cloud","ManageLevel":"product","ManagedBy":"terraform","TerraformCloudWorkspace":"2-products_product-1_dev-1_ingress","TerraformModuleSource":"dasmeta/modules/aws//modules/ingress","TerraformModuleVersion":"1.1.4"}
+    tags = { "Account" : "boilerplate", "AppliedFrom" : "terraform-cloud", "ManageLevel" : "product", "ManagedBy" : "terraform", "TerraformCloudWorkspace" : "2-products_product-1_dev-1_ingress", "TerraformModuleSource" : "dasmeta/modules/aws//modules/ingress", "TerraformModuleVersion" : "1.1.4" }
   }
 }
 provider "kubernetes" {
 
 
-  cluster_ca_certificate = "${data.tfe_outputs.this[\"1-environments/dev-1/eks"].values.results.ca_certificate}"
-  host = "${data.tfe_outputs.this["1-environments/dev-1/eks"].values.results.host}"
-  token = "${data.tfe_outputs.this["1-environments/dev-1/eks"].values.results.token}"
+  cluster_ca_certificate = data.tfe_outputs.this["1-environments/dev-1/eks"].values.results.ca_certificate
+  host                   = data.tfe_outputs.this["1-environments/dev-1/eks"].values.results.host
+  token                  = data.tfe_outputs.this["1-environments/dev-1/eks"].values.results.token
 
   exec {
 
 
-api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1beta1"
 
-args = ["eks","--region","eu-central-1","get-token","--cluster-name","local.cluster_name"]
+    args = ["eks", "--region", "eu-central-1", "get-token", "--cluster-name", "local.cluster_name"]
 
-command = "aws"
+    command = "aws"
 
   }
 
